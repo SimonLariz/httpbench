49e3d37d34f6685e08e71ef22e00574d832d8597
Simon Lariz <simonlariz@gmail.com>
2024-06-02 11:08:48 -0700

    more project structure

---
+++ pkg/benchmark/benchmark.go
@@ -0,0 +1,4 @@
+// Core benchmarking functionality
+// Function takes URL and sends HTTP requests to the URL using net/http package
+// Implement logic for response time calculation
+// Use goroutines to send multiple requests concurrently
---
+++ pkg/metrics/metrics.go
@@ -0,0 +1,3 @@
+// Define data structures for storing and calculating benchmark results
+// Create functions for calculating metrics like latency, throughput, and error rate
+// Create functions for aggregating and printing benchmark results
