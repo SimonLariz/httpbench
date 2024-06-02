2f2d16003a6752b732f381653a5a431d5283d6cd
Simon Lariz <simonlariz@gmail.com>
2024-06-02 11:04:11 -0700

    init commit project structure

---
+++ cmd/httpbench/main.go
@@ -0,0 +1,3 @@
+// Define command line flags for benchmarking using the flag package
+// Parse the command line arguments to ge the user-provided values for URL, number of requests, concurrency, and timeout
+// Validate input and handle errors
